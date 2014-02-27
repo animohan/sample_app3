acec7f46353af3fefdbf555576b585173918b686
animohan <anishmohan@gmail.com>
2014-02-27 06:12:15 -0800

    fixed root_path issue in home page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,9 +4,9 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the content 'Sample App3'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App3')
+    it "should have the content 'Sample App'" do
+      visit root_path
+      expect(page).to have_content('Sample App')
     end
 
     it "should have the base title" do
